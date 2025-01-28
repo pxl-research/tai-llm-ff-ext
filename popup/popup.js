f40ff070b5932d4f104754c45b2c20ee2da891dc
Servaas Tilkin <31237936+stilkin-pxl@users.noreply.github.com>
2025-01-28 16:26:32 +0100

    CHANGE: changed layout of pop-up

--- popup/popup.js
+++ popup/popup.js
@@ -1,33 +1,27 @@
 function listenForClicks() {
-    document.addEventListener("click", (e) => {
-        console.log('click event: ' + e.target.textContent)
+    document.addEventListener('click', (e) => {
 
-        // Send a message to the content script in the active tab.
+        // get user entered data
+        const txUserData = document.getElementById('user_data');
+
+        // send a message to the content script in the active tab.
         function performAction(tabs) {
-            console.log('Tabs: ' + tabs);
             browser.tabs.sendMessage(tabs[0].id, {
-                command: "logFormElements",
-                target: e.target.textContent
+                command: 'logFormElements',
+                target: e.target.textContent,
+                userData: txUserData.value
             });
-            //     browser.tabs.insertCSS({code: hidePage}).then(() => {
-            //         const url = beastNameToURL(e.target.textContent);
-            //         browser.tabs.sendMessage(tabs[0].id, {
-            //             command: "beastify",
-            //             beastURL: url,
-            //         });
-            //     });
         }
 
         function reportError(error) {
             console.error(`Error: ${error}`);
         }
 
-        // Get the active tab, then call a method as appropriate.
-        if (e.target.tagName !== "BUTTON" || !e.target.closest("#popup-content")) {
-            // Ignore when click is not on a button within <div id="popup-content">.
+        // get the active tab, then call a method as appropriate.
+        if (e.target.tagName !== 'BUTTON' || !e.target.closest('#popup-content')) {
+            // ignore when click is not on a button within <div id='popup-content'>.
             return;
         } else {
-            console.log('popup action');
             browser.tabs
                 .query({active: true, currentWindow: true})
                 .then(performAction)
@@ -36,17 +30,17 @@ function listenForClicks() {
     });
 }
 
-// Display the popup's error message and hide the normal UI.
+// display the popup's error message and hide the normal UI
 function reportExecuteScriptError(error) {
-    document.querySelector("#popup-content")
-        .classList.add("hidden");
-    document.querySelector("#error-content")
-        .classList.remove("hidden");
-    console.error(`Failed to execute beastify content script: ${error.message}`);
+    document.querySelector('#popup-content')
+        .classList.add('hidden');
+    document.querySelector('#error-content')
+        .classList.remove('hidden');
+    console.error(`Failed to execute content script: ${error.message}`);
 }
 
-// When the popup loads, inject the content script into the active tab.
+// when the popup loads, inject the content script into the active tab
 browser.tabs
-    .executeScript({file: "/content_scripts/content_script.js"})
+    .executeScript({file: '/content_scripts/content_script.js'})
     .then(listenForClicks)
     .catch(reportExecuteScriptError);
