8a6d145e4a2d06bd9fae10ceaf5e6ca33abab897
Servaas Tilkin <31237936+stilkin-pxl@users.noreply.github.com>
2025-02-20 13:39:22 +0100

    CHANGE: moved the LLM methods to a separate file

---
+++ scripts/llm_tools.js
@@ -0,0 +1,49 @@
+// OPENROUTER CALLS
+const baseUrl = 'https://openrouter.ai/api/v1/chat/completions';
+const defaultHeaders = {
+    'HTTP-Referer': 'https://pxl-firefox-plugin.be/',
+    'X-Title': 'Firefox LLM Plug-In @ PXL Smart ICT',
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*'
+};
+const defaultModel = 'google/gemini-2.0-flash-001';
+
+const systemPrompt = {
+    'role': 'system',
+    'content': 'You are an assistant who has to help people fill out web forms. ' +
+        'You will receive some JSON text with the following information: ' +
+        ' - a list of webpage elements (such as form input fields or text data)  ' +
+        ' with their tag name, text value, and path in the DOM. ' +
+        ' - a (potentially large) block of text that may contain information to fill out the form ' +
+        'Please return a valid JSON array with the following information: ' +
+        ' - the "path" of the element ' +
+        ' - the suggested "value" for the element ' +
+        'If you want to add additional information you may add a "remark" field to pass it on, ' +
+        'but always ensure the result is a valid JSON array. ' +
+        'Make absolutely sure you DO NOT include any markup such as ```, ```json or any other markup. '
+};
+
+async function callOpenRouter(messages, apiKey, modelStr = defaultModel) {
+    const headers = defaultHeaders;
+    headers['Authorization'] = `Bearer ${apiKey}`;
+
+    const body = {
+        'model': modelStr,
+        'messages': messages
+    };
+
+    try {
+        const response = await fetch(baseUrl, {
+            method: 'POST',
+            headers: headers,
+            body: JSON.stringify(body)
+        });
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        return await response.json();
+    } catch (error) {
+        console.error(error.message);
+    }
+}
