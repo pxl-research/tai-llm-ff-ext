a2c2d0576a6e2745c4cf208c9b1c86cc8e79ee70
Servaas Tilkin <31237936+stilkin-pxl@users.noreply.github.com>
2025-02-24 13:52:12 +0100

    FEAT: pop-up now shows (and retains) input suggestions based on LLM

--- popup/popup.js
+++ popup/popup.js
@@ -1,15 +1,22 @@
+const LS_OR_KEY = 'or_key';
+const LS_USER_DATA = 'user_data';
+const LS_OUTPUT = 'output';
+
 function listenForClicks() {
     const CLICK = 'click';
 
-    const txOrKey = document.getElementById('or_key');
-    const txUserData = document.getElementById('user_data');
-    txOrKey.value = localStorage.getItem('or_key');
+    const txOrKey = document.getElementById(LS_OR_KEY);
+    const txUserData = document.getElementById(LS_USER_DATA);
+    const divOutput = document.getElementById(LS_OUTPUT);
+
+    txOrKey.value = localStorage.getItem(LS_OR_KEY);
+    txUserData.value = localStorage.getItem(LS_USER_DATA);
+    divOutput.innerHTML = localStorage.getItem(LS_OUTPUT);
 
     document.addEventListener(CLICK, (e) => {
 
         // send a message to the content script in the active tab.
         function performAction(tabs) {
-
             if (e.target.id === 'fill_out_form') {
                 // get user entered data and pass to content script
                 browser.tabs.sendMessage(tabs[0].id,
@@ -20,9 +27,16 @@ function listenForClicks() {
                         userData: txUserData.value.trim(),
                         apiKey: txOrKey.value.trim()
                     });
+
+                // store user data in local storage, for convenience
+                localStorage.setItem(LS_USER_DATA, txUserData.value.trim());
+
+                const outputList = document.getElementById('output');
+                outputList.innerHTML = ''; // clear response
+
             } else if (e.target.id === 'save_or_key') {
                 // store the key in local storage TODO: encrypt
-                localStorage.setItem('or_key', txOrKey.value.trim());
+                localStorage.setItem(LS_OR_KEY, txOrKey.value.trim());
             }
         }
 
@@ -51,22 +65,49 @@ function reportExecuteScriptError(error) {
     console.error(`Failed to execute content script: ${error.message}`);
 }
 
-browser.runtime.onMessage.addListener((message, sender) => {
-    console.log(`browser.runtime.onMessage: ${JSON.stringify(message)}`);
 
+function processMessage(message, state) {
+    try {
+        const msgObj = JSON.parse(message);
+
+        if (msgObj.hasOwnProperty('label')) {
+            // display the message as an input suggestion
+            const outputList = document.getElementById('output');
+            outputList.innerHTML += `<dt>${msgObj.label}</dt>\n`;
+            outputList.innerHTML += `<dd>${msgObj.value}</dd>\n`;
+
+            // store this to preserve state
+            localStorage.setItem(LS_OUTPUT, outputList.innerHTML.trim());
+        }
+    } catch (error) {
+        console.warn(`Could not process content of message: ${error}`);
+    }
+}
+
+// copy to clipboard
+function c2cb(text) {
+    navigator.clipboard.writeText(text);
+}
+
+
+// register a listener for communication with the script in the tab
+browser.runtime.onMessage.addListener((message, sender) => {
+    console.log(`browser.runtime.onMessage from ${message.target}`);
 
     const progressBar = document.getElementById('progress_bar');
     switch (message.state) {
         case 1: // running
             progressBar.style.display = 'inline-block';
+            const debugMsg = document.getElementById('debug_msg');
+            debugMsg.innerText = message.message;
             break;
-        default: // not running
+        case 2: // done
             progressBar.style.display = 'none';
             break;
+        default: // other state
+            processMessage(message.message, message.state);
+            break;
     }
-
-    const debugMsg = document.getElementById('debug_msg');
-    debugMsg.innerText = message.message;
 });
 
 // when the popup loads, inject the content script into the active tab
